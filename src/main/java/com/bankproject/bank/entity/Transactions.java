11a1f580f78118740049c087552700d7fe353121
Michal0ss <michal.bialas.pol@gmail.com>
2026-03-29 21:58:20 +0200

    craeted db entity for transactions table

--- src/main/java/com/bankproject/bank/entity/Transactions.java
+++ src/main/java/com/bankproject/bank/entity/Transactions.java
@@ -1,5 +1,101 @@
 package com.bankproject.bank.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
 public class Transactions {
-//    TODO -> wypelnic
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_account_id", referencedColumnName = "accountId")
+    private Accounts fromAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_account_id", referencedColumnName = "accountId")
+    private Accounts toAccount;
+
+    private BigDecimal amount;
+
+    @Column(name = "transaction_type")
+    private String transactionType;
+
+    private LocalDateTime createdAt;
+
+    public Transactions(Long transactionId, Accounts fromAccount, Accounts toAccount, BigDecimal amount, String transactionType, LocalDateTime createdAt) {
+        this.transactionId = transactionId;
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.createdAt = createdAt;
+    }
+
+    public Transactions() {}
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Accounts getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(Accounts fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public Accounts getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(Accounts toAccount) {
+        this.toAccount = toAccount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Transactions{" +
+                "transactionId=" + transactionId +
+                ", fromAccount=" + fromAccount +
+                ", toAccount=" + toAccount +
+                ", amount=" + amount +
+                ", transactionType='" + transactionType + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
