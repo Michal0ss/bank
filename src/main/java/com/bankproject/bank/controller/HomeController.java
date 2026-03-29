d4bc3a786a7b8538dce4fdf1e6a5f554ddc8dbba
Michal0ss <michal.bialas.pol@gmail.com>
2026-03-29 23:05:03 +0200

    created html templates with css styling and connected with controller

--- src/main/java/com/bankproject/bank/controller/HomeController.java
+++ src/main/java/com/bankproject/bank/controller/HomeController.java
@@ -8,6 +8,16 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
         return "login";
+    }
+
+    @GetMapping("register")
+    public String register(){
+        return "register";
     }
 }
